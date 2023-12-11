9413d17bed7830ec1d3d99bf5de93624ccfe5c92
Johnathan Bailey <whoisjohna1@gmailc.om>
2023-12-11 13:37:46 -0500

    Initial commit

--- app/src/main/java/com/example/a20141251majorprojectjohnathanbailey/Courses.kt
+++ app/src/main/java/com/example/a20141251majorprojectjohnathanbailey/Courses.kt
@@ -1,8 +1,8 @@
 package com.example.a20141251majorprojectjohnathanbailey
 
-data class Courses (val Course_code: String? = null, val Course_Title: String? = null,
-                    val Credits: String? = null, val Description: String? = null,
-                    val Pre_requisites: String? = null) {
+data class Courses (val coursecode: String? = null, val courseTitle: String? = null,
+                    val credits: String? = null, val description: String? = null,
+                    val prerequisites: String? = null) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
 }
